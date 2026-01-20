f1e1ae68a832fa21f6e44ab17b6963bf828bf5e3
wanna-dev <dev@wannna.ai>
2026-01-20 16:35:04 +0100

    first commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
 
   return (
     <>
+      
       <div className="container">
           <p className="description">ART-DRIVEN CONSULTING AND PRODUCTION FOR BRANDS, GALLERIES AND CREATIVE LEADERS</p>
           <div className="container-footer">
