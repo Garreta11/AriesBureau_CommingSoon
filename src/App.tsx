50c0b0b5d1352ab43fbfc1f6b43efbbd08927c7f
wanna-dev <dev@wannna.ai>
2026-01-20 16:33:10 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+          <p className="description">ART-DRIVEN CONSULTING AND PRODUCTION FOR BRANDS, GALLERIES AND CREATIVE LEADERS</p>
+          <div className="container-footer">
+              <img src="./logo-black.svg" alt="Random Image" />
+          </div>
+      </div>
+
+      <div className="footer">
+          <p>website coming soon</p>
+          <a href="mailto:adele@ariesbureau.com" target="_blank" className="email">
+              adele@ariesbureau.com
+          </a>
+      </div>
+    </>
+  );
+}
+
+export default App;
