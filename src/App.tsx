c4cd3fbbe46374c4ae0e7a7b9f5ddaf3c3edcf9b
garreta11 <jordigarreta11@gmail.com>
2026-02-26 09:44:56 +0100

    add new design

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,21 @@ function App() {
   return (
     <>
       <div className="container">
-          <p className="description">ART-DRIVEN CONSULTING AND PRODUCTION FOR BRANDS, GALLERIES AND CREATIVE LEADERS</p>
+          {/* <p className="description">ART-DRIVEN CONSULTING AND PRODUCTION FOR BRANDS, GALLERIES AND CREATIVE LEADERS</p> */}
+          <p className="description">
+            Ariès Bureau is a boutique consultancy specialised in art-driven consulting and creative production.
+            <br />
+            We work with collectors, galleries, creative leaders, and brands to turn ideas into clear strategies and tangible experiences. We curate collections, develop concepts, and lead projects from vision to reality, combining artistic sensitivity with structured project management.
+            <br />
+            <br />
+            Our approach is organised into three key phases — curating, developing and delivering — that shape our practice. This may involve building or refining an art collection, conceiving and producing exhibitions, managing events, guiding the launch and growth of creative projects, or coordinating multidisciplinary teams toward a shared objective. While we operate across defined areas of expertise, every engagement is conceived in response to a specific context and tailored to the client’s ambitions, scale, and environment.
+            <br />
+            <br />
+            At Ariès Bureau, we believe in quality, professionalism, curiosity, and meaning. Every project is approached with rigor and intuition, from shaping narratives to managing teams and guiding complex productions. Our approach is hands-on, strategic and precise: we make things happen, turning ideas into results that are beautiful, coherent, and operationally effective.
+          </p>
+
+          <p className="description">For more information please reach out to <a href="mailto:adele@ariesbureau.com" target="_blank" className="email">adele@ariesbureau.com</a></p>
+
           <div className="container-footer">
               <img src="./logo-black.svg" alt="Random Image" />
           </div>
